fb98d1fd5efc3e159d612d1bb438fbdbb5bdd5ae
Marek Bojęć <marek.bojec@deviniti.com>
2023-11-14 16:02:39 +0100

    Update flutter to 3.13.9

--- core/modules/presentation/lib/environment/env.dart
+++ core/modules/presentation/lib/environment/env.dart
@@ -7,5 +7,5 @@ part 'env.g.dart';
 @Envied(path: 'lib/environment/.env', obfuscate: true)
 abstract class Env {
   @EnviedField(varName: 'API_URL')
-  static final apiUrl = _Env.apiUrl;
+  static final String apiUrl = _Env.apiUrl;
 }
